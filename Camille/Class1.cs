b68de8ec3ad6fcbd37d910450ac44cb34bc2b011
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2018-01-01 22:42:55 -0800

    project init

---
+++ Camille/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+
+namespace MingweiSamuel.Camille
+{
+    public class Class1
+    {
+        void test()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var json = httpClient.GetStringAsync("url").Result;
+
+                // Now parse with JSON.Net
+            }
+        }
+    }
+}
