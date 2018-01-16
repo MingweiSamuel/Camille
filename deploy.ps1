60edd805f6425712b717576a90c9574b652bea73
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2018-01-16 09:55:23 -0800

    deploy to nuget

--- deploy.ps1
+++ deploy.ps1
@@ -1,15 +1,12 @@
 Push-Location .\Camille
 
  [xml]$CSPROJ = Get-Content -Path .\Camille.csproj
- $VERSION = $CSPROJ.Project.PropertyGroup.Version
- $NEW_VERSION = "$VERSION-nightly-$ENV:APPVEYOR_REPO_COMMIT"
+ $NEW_VERSION = "$($CSPROJ.Project.PropertyGroup.Version)-nightly-$($ENV:APPVEYOR_REPO_COMMIT.subString(0, 10))"
 
  Write-Host "version: $NEW_VERSION"
 
  dotnet msbuild /t:build /p:Configuration=Release
  dotnet msbuild /t:pack /p:Configuration=Release /p:Version="$NEW_VERSION" /p:PackageReleaseNotes="Nightly Release"
 
- # publish
- Get-ChildItem .\bin\Release\*.nupkg | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
  Pop-Location
