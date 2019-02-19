13f0ab5a9765216a64d6faa987388b1d4a82d75f
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-02-19 01:07:00 -0800

    adding summoner match history test

--- Camille.Test/ReadmeExampleV4Test.cs
+++ Camille.Test/ReadmeExampleV4Test.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MingweiSamuel.Camille.Enums;
 
@@ -8,7 +10,7 @@ namespace Camille.Test
     public class ReadmeExampleTest : ApiTest
     {
         [TestMethod]
-        public void SummonerChampionMasteryTest()
+        public void SummonerChampionMastery()
         {
             // Use existing instance for test.
             var riotApi = Api;
@@ -37,6 +39,60 @@ namespace Camille.Test
                         mastery.ChampionPoints, mastery.ChampionLevel);
                 }
                 Console.WriteLine();
+            }
+        }
+
+        [TestMethod]
+        public async Task SummonerMatchHistoryAsync()
+        {
+            // Use existing instance for test.
+            var riotApi = Api;
+
+            var summonerNameQuery = "lugnutsk";
+
+            // Get summoners data (blocking).
+            var summonerData = await riotApi.SummonerV4.GetBySummonerNameAsync(Region.NA, summonerNameQuery);
+            if (null == summonerData)
+            {
+                // If a summoner is not found, the response will be null.
+                Console.WriteLine($"Summoner '{summonerNameQuery}' not found.");
+                return;
+            }
+
+            Console.WriteLine($"Match history for {summonerData.Name}:");
+
+            // Get 10 most recent matches (blocking).
+            // Queue ID 420 is RANKED_SOLO_5v5 (TODO)
+            var matchlist = await riotApi.MatchV4.GetMatchlistAsync(
+                Region.NA, summonerData.AccountId, queue: new[] { 420 }, endIndex: 10);
+            // Get match results (done asynchronously -> not blocking -> fast).
+            var matchDataTasks = matchlist.Matches.Select(
+                    matchMetadata => riotApi.MatchV4.GetMatchAsync(Region.NA, matchMetadata.GameId)
+                ).ToArray();
+            // Wait for all task requests to complete asynchronously.
+            var matchDatas = await Task.WhenAll(matchDataTasks);
+
+            for (var i = 0; i < matchDatas.Count(); i++)
+            {
+                var matchData = matchDatas[i];
+                // Get this summoner's participant ID info.
+                var participantIdData = matchData.ParticipantIdentities
+                    .First(pi => summonerData.Id.Equals(pi.Player.SummonerId));
+                // Find the corresponding participant.
+                var participant = matchData.Participants
+                    .First(p => p.ParticipantId == participantIdData.ParticipantId);
+
+                var win = participant.Stats.Win;
+                var champ = (Champion) participant.ChampionId;
+                var k = participant.Stats.Kills;
+                var d = participant.Stats.Deaths;
+                var a = participant.Stats.Assists;
+                var kda = (k + a) / (float) d;
+
+                // Print #, win/loss, champion.
+                Console.WriteLine("{0,3}) {1,-4} ({2})", i + 1, win ? "Win" : "Loss", champ.Name());
+                // Print champion, K/D/A
+                Console.WriteLine("     K/D/A {0}/{1}/{2} ({3:0.00})", k, d, a, kda);
             }
         }
     }
