a4d9af3a5156aecee465c738f9da2e86415de5f4
Roderick Sieben <r.sieben@genxs.net>
2019-10-17 14:39:13 +0200

    Added new generated enums

--- Camille/gen/download.ps1
+++ Camille/gen/download.ps1
@@ -6,3 +6,11 @@ $DD = ((Invoke-WebRequest -Uri "http://ddragon.leagueoflegends.com/realms/na.jso
 echo "DD version $DD."
 
 Invoke-WebRequest -Uri "http://ddragon.leagueoflegends.com/cdn/$DD/data/en_US/champion.json" -OutFile .\.champion.json
+
+echo "Downloading static data"
+
+Invoke-WebRequest -Uri "http://static.developer.riotgames.com/docs/lol/seasons.json" -OutFile .\Enums\.seasons.json
+Invoke-WebRequest -Uri "http://static.developer.riotgames.com/docs/lol/queues.json" -OutFile .\Enums\.queues.json
+Invoke-WebRequest -Uri "http://static.developer.riotgames.com/docs/lol/maps.json" -OutFile .\Enums\.maps.json
+Invoke-WebRequest -Uri "http://static.developer.riotgames.com/docs/lol/gameModes.json" -OutFile .\Enums\.gameModes.json
+Invoke-WebRequest -Uri "http://static.developer.riotgames.com/docs/lol/gameTypes.json" -OutFile .\Enums\.gameTypes.json
