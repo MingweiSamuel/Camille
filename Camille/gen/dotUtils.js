11e3b063d4d171e60290f96af0f5bb575cea80a4
Roderick Sieben <r.sieben@genxs.net>
2019-11-05 10:05:27 +0100

    removed leftover js logic

--- Camille/gen/dotUtils.js
+++ Camille/gen/dotUtils.js
@@ -37,7 +37,7 @@ function normalizePropName(propName, schemaName, value) {
   return name;
 }
 
-function stringifyType(prop, name = '', endpoint = null, nullable = false) {
+function stringifyType(prop, endpoint = null, nullable = false) {
   if (prop.anyOf) {
     prop = prop.anyOf[0];
   }
@@ -61,10 +61,10 @@ function stringifyType(prop, name = '', endpoint = null, nullable = false) {
     case 'boolean': return 'bool' + qm;
     case 'integer': return ('int32' === prop.format ? 'int' : 'long') + qm;
     case 'number': return prop.format + qm;
-    case 'array': return stringifyType(prop.items, name, endpoint) + '[]' + qm;
+    case 'array': return stringifyType(prop.items, endpoint) + '[]' + qm;
     case 'object':
-      return 'IDictionary<' + stringifyType(prop['x-key'], name, endpoint) + ', ' +
-        stringifyType(prop.additionalProperties, name, endpoint) + '>' + qm;
+      return 'IDictionary<' + stringifyType(prop['x-key'], endpoint) + ', ' +
+        stringifyType(prop.additionalProperties, endpoint) + '>' + qm;
     default: return prop.type + qm;
   }
 }
