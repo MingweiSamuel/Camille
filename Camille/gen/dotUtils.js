2ac7e6aff09da67b22cec550362c3a6dfe1ab648
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-11-06 13:35:11 -0800

    Fix JS template sort order, fix #21

--- Camille/gen/dotUtils.js
+++ Camille/gen/dotUtils.js
@@ -1,7 +1,21 @@
 ﻿// flatMap: https://gist.github.com/samgiles/762ee337dff48623e729
 // [B](f: (A) ⇒ [B]): [B]  ; Although the types in the arrays aren't strict (:
-Array.prototype.flatMap = function (lambda) {
+Array.prototype.flatMap = function(lambda) {
   return Array.prototype.concat.apply([], this.map(lambda));
+};
+
+Array.prototype.sortBy = function(lambda) {
+  return this.sort((a, b) => {
+    const va = lambda(a);
+    const vb = lambda(b);
+    if ((typeof va) !== (typeof vb))
+      throw Error(`Mismatched sort types: ${typeof va}, ${typeof vb}.`);
+    if (typeof va === 'number')
+      return va - vb;
+    if (typeof va === 'string')
+      return va.localeCompare(vb);
+    throw Error(`Unknown sort type: ${typeof va}.`);
+  });
 };
 
 function capitalize(input) {
