8b979242b2c75831e8c8912106dba4ea7c812283
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-11-11 00:43:02 -0800

    adding lcu method gen (not yet working)

---
+++ Camille.Lcu/src/Endpoints.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Camille.Lcu
+{
+    public abstract class Endpoints
+    {
+        protected static readonly IEnumerable<KeyValuePair<string, string>> QUERY_PARAMS_EMPTY =
+            Enumerable.Empty<KeyValuePair<string, string>>();
+
+        protected readonly Lcu Lcu;
+
+        protected Endpoints(Lcu lcu)
+        {
+            Lcu = lcu;
+        }
+    }
+}
