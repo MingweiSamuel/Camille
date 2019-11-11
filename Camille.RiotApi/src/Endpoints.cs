f6d6c5eb1a225c3b9ac349738334b6046ba514af
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-11-11 02:32:10 -0800

    Handle request body, fix #17
    TODO: share namespace between tournament STUB and tournament

--- Camille.RiotApi/src/Endpoints.cs
+++ Camille.RiotApi/src/Endpoints.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Runtime.CompilerServices;
 
 namespace Camille.RiotApi
 {
     public abstract class Endpoints
     {
-        protected static readonly IEnumerable<KeyValuePair<string, string>> QUERY_PARAMS_EMPTY =
-            Enumerable.Empty<KeyValuePair<string, string>>();
-
         protected readonly RiotApi RiotApi;
 
         protected Endpoints(RiotApi riotApi)
         {
             RiotApi = riotApi;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected static string JsonSerialize(object value)
+        {
+#if USE_NEWTONSOFT
+            return Newtonsoft.Json.JsonConvert.SerializeObject(value);
+#endif
+#if USE_SYSTEXTJSON
+            return System.Text.Json.JsonSerializer.Serialize(value);
+#endif
         }
     }
 }
