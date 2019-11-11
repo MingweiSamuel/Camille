580fbfbef6a6bd5ca8a8c349da662906a1b5fb1c
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-11-11 13:22:58 -0800

    Change Tier.UNRAKED -> NONE, used in LCU, add numerical

--- Camille.Enums/src/Tier.cs
+++ Camille.Enums/src/Tier.cs
@@ -6,19 +6,19 @@
 #if USE_SYSTEXTJSON
   [System.Text.Json.Serialization.JsonConverter(typeof(System.Text.Json.Serialization.JsonStringEnumConverter))]
 #endif
-    public enum Tier
+    public enum Tier : byte
     {
-        CHALLENGER,
-        GRANDMASTER,
-        MASTER,
-        DIAMOND,
-        PLATINUM,
-        GOLD,
-        SILVER,
-        BRONZE,
-        IRON,
+        CHALLENGER  =  20,
+        GRANDMASTER =  40,
+        MASTER      =  60,
+        DIAMOND     =  80,
+        PLATINUM    = 100,
+        GOLD        = 120,
+        SILVER      = 140,
+        BRONZE      = 160,
+        IRON        = 180,
 
-        /// <summary>In most endpoints, tier will not be provided if related summoner is unranked.</summary>
-        UNRANKED,
+        /// <summary>Used by some LCU endpoints.</summary>
+        NONE = byte.MaxValue,
     }
 }
