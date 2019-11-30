18229405ba9f14af9c8322335192eb37df27c090
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-11-29 17:59:04 -0800

    Wamp interface based on event callbacks

--- Camille.Lcu.Test/UnitTest1.cs
+++ Camille.Lcu.Test/UnitTest1.cs
@@ -1,9 +1,6 @@
+using Camille.Lcu.Util;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Net;
-using System.Net.WebSockets;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Camille.Lcu.Test
@@ -12,46 +9,42 @@ namespace Camille.Lcu.Test
     //[Ignore("Required LCU to be running.")]
     public class UnitTest1
     {
-        /*[TestMethod]
-        public async Task TestWamp()
+        [TestMethod]
+        public async Task TestLcuRest()
         {
-            var lockfile = Lockfile.GetFromProcess();
-            var uri = new UriBuilder("wss", "127.0.0.1", lockfile.Port).Uri;
 
-            var client = new ClientWebSocket();
-            client.Options.AddSubProtocol("wamp");
-            client.Options.Credentials = new NetworkCredential("riot", lockfile.Password);
-            client.Options.RemoteCertificateValidationCallback = new LcuConfig().CertificateValidationCallback;
-
-            await client.ConnectAsync(uri, CancellationToken.None);
-
-            var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes("[5, \"OnJsonApiEvent\"]"));
-            await client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
-
-            var bufferArr = new byte[4096];
-            var buffer2 = new ArraySegment<byte>(bufferArr);
-            var result = await client.ReceiveAsync(buffer2, CancellationToken.None);
-
-            Console.WriteLine(result.Count);
-            Console.WriteLine(Encoding.UTF8.GetString(bufferArr, 0, result.Count));
-        }*/
+        }
 
         [TestMethod]
         public async Task TestMethod1()
         {
             var lockfile = Lockfile.GetFromProcess();
-            var lcu = new Lcu(lockfile);
+            using var lcu = new Lcu(lockfile);
+            var wamp = lcu.wamp;
 
-            await lcu.Connect();
+            wamp.OnConnect += async () =>
+            {
+                Console.WriteLine("Connected!");
+                await wamp.Subscribe("OnJsonApiEvent_lol-login_v1_session", async (data) =>
+                {
+                    Console.WriteLine("Logged in: " + data);
+                    await wamp.Close();
+                    return HandlerResult.Success;
+                });
+            };
+            wamp.OnDisconnect += () =>
+            {
+                Console.WriteLine("Disconnected!");
+                return null;
+            };
+
+            wamp.Connect();
 
             var obj = await lcu.LolLogin().PostSessionV1Async(new LolLogin.UsernameAndPassword
             {
                 Username = "0x5A79",
-                Password = "wrong password",
+                Password = "wrongpassword",
             });
-            //var obj = await lcu.Send<object>(new HttpRequestMessage(HttpMethod.Get, "/swagger/v2/openapi.json"));
-
-            Console.WriteLine(obj);
         }
     }
 }
---
+++ Camille.Lcu/src/Util/WampMessageType.cs
@@ -0,0 +1,15 @@
+﻿namespace Camille.Lcu.Util
+{
+    enum WampMessageType : int
+    {
+        Welcome = 0,
+        Prefix = 1,
+        Call = 2,
+        CallResult = 3,
+        CallError = 4,
+        Subscribe = 5,
+        Unsubscribe = 6,
+        Publish = 7,
+        Event = 8
+    }
+}
