48996c6410a4970646616bfd84e7f634fcf2743a
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-04-20 21:54:31 -0700

    npm install in separate ps1, mutex to prevent concurrent install errors [no deploy]

---
+++ srcgen/install.ps1
@@ -0,0 +1,26 @@
+Set-Location $PSScriptRoot
+
+$mtx = New-Object System.Threading.Mutex($False, 'CamilleNpmMutex')
+
+If (-Not ($mtx.WaitOne(30000))) {
+    Write-Output 'Failed to acquire mutex.'
+    Exit 10
+}
+
+Try {
+    If ((Test-Path 'node_modules') -And ((Get-Item 'node_modules')[0].LastWriteTime `
+        -GE (Get-Item 'package-lock.json')[0].LastWriteTime))
+    {
+        Write-Output 'No npm install needed.'
+    }
+    Else {
+        If ([Version]$(npm -v) -GE [Version]'5.7.0') {
+            npm ci --loglevel error
+        } Else {
+            npm install
+        }
+    }
+}
+Finally {
+    $mtx.ReleaseMutex()
+}
