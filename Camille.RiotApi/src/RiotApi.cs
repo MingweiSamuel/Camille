7843b541fb00e8fc25765f48217922d03fa072ba
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-08-23 13:38:55 -0700

    Add customer int converters to handle 123.0, fixes #39
    https://github.com/RiotGames/developer-relations/issues/345

--- Camille.RiotApi/src/RiotApi.cs
+++ Camille.RiotApi/src/RiotApi.cs
@@ -4,12 +4,75 @@ using System.Threading;
 using System.Threading.Tasks;
 using Camille.RiotApi.Util;
 
+#if USE_NEWTONSOFT
+#elif USE_SYSTEXTJSON
+#else
+#error Must have one of USE_NEWTONSOFT or USE_SYSTEXTJSON set.
+#endif
+
 namespace Camille.RiotApi
 {
     public class RiotApi : IRiotApi
     {
         /// <summary>RequestManager for sending web requests and managing rate limits.</summary>
         private readonly RequestManager _requestManager;
+
+
+#if USE_NEWTONSOFT
+        private static readonly CustomIntConverter customIntConverter = new CustomIntConverter();
+        private class CustomIntConverter : Newtonsoft.Json.JsonConverter
+        {
+            public override bool CanConvert(Type objectType) { return typeof(int) == objectType; }
+
+            public override object ReadJson(
+                Newtonsoft.Json.JsonReader reader, Type objectType,
+                object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+            {
+                if (Newtonsoft.Json.JsonToken.Float == reader.TokenType)
+                {
+                    var doubleVal = serializer.Deserialize<double>(reader);
+                    var intVal = (int) doubleVal;
+                    if (doubleVal == intVal)
+                        return intVal;
+                    throw new Newtonsoft.Json.JsonException($"Cannot parse number as int: {doubleVal}.");
+                }
+                return (int) serializer.Deserialize<long>(reader);
+            }
+
+            public override bool CanWrite { get { return false; } }
+
+            public override void WriteJson(
+                Newtonsoft.Json.JsonWriter writer, object value,
+                Newtonsoft.Json.JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
+        }
+#elif USE_SYSTEXTJSON
+        private static readonly System.Text.Json.JsonSerializerOptions jsonOptions = new System.Text.Json.JsonSerializerOptions();
+        private class CustomIntConverter : System.Text.Json.Serialization.JsonConverter<int>
+        {
+            public override int Read(ref System.Text.Json.Utf8JsonReader reader, Type type, System.Text.Json.JsonSerializerOptions options)
+            {
+                var valDouble = reader.GetDouble();
+                var valInt = (int) valDouble;
+                if (valDouble == valInt)
+                    return valInt;
+                return reader.GetInt32();
+            }
+
+            public override void Write(System.Text.Json.Utf8JsonWriter writer, int value, System.Text.Json.JsonSerializerOptions options)
+            {
+                writer.WriteNumberValue(value);
+            }
+        }
+
+        static RiotApi()
+        {
+            jsonOptions.Converters.Add(new CustomIntConverter());
+        }
+#endif
+
 
         private RiotApi(IRiotApiConfig config)
         {
@@ -47,11 +110,9 @@ namespace Camille.RiotApi
             var content = await _requestManager.Send(route, methodId, request, token.GetValueOrDefault(), ignoreAppRateLimits);
             if (null == content) return default!; // TODO: throw exception on unexpected null.
 #if USE_NEWTONSOFT
-            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(content);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(content, customIntConverter);
 #elif USE_SYSTEXTJSON
-            return System.Text.Json.JsonSerializer.Deserialize<T>(content);
-#else
-#error Must have one of USE_NEWTONSOFT or USE_SYSTEXTJSON set.
+            return System.Text.Json.JsonSerializer.Deserialize<T>(content, jsonOptions);
 #endif
         }
 
