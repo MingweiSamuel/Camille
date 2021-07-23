14aba191c993914059956cd8aad029ba2e7fce2f
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-23 15:01:24 -0700

    Add match-v5 test (disabled for CI API key)
    https://github.com/MingweiSamuel/Camille/issues/57

---
+++ Camille.RiotGames.Test/ApiMatchV5Test.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Camille.Enums;
+using System.Linq;
+
+
+namespace Camille.RiotGames.Test
+{
+    [TestClass]
+    [Ignore("No access with CI API key")]
+    public class ApiMatchV5Test : ApiTest
+    {
+        public static string[] MatchIds =
+        {
+            "EUW1_5378349967",
+        };
+
+        [TestMethod]
+        public async Task GetMatchAsync()
+        {
+            var tasks = MatchIds
+                .Select(id => Api.MatchV5().GetMatchAsync(RegionalRoute.EUROPE, id))
+                .ToList();
+            var matches = await Task.WhenAll(tasks);
+
+            foreach (var ( match, id ) in matches.Zip(MatchIds, (match, id) => ( match, id )))
+            {
+                Assert.IsNotNull(match);
+                Assert.AreEqual(id, match.Metadata.MatchId);
+                Assert.IsTrue(id.EndsWith("" + match.Info.GameId), $"Requested match id {id}'s gameId is {match.Info.GameId} unexpectedly.");
+
+                Assert.AreEqual(2, match.Info.Teams.Length);
+
+                Assert.IsNotNull(match.Info.Participants);
+
+                foreach (var participant in match.Info.Participants)
+                {
+                    Assert.IsNotNull(participant);
+                    Assert.IsTrue(0 < participant.ChampionId);
+                    Assert.IsNotNull(participant.ParticipantId);
+                    Assert.IsNotNull(participant.SummonerId);
+                    Assert.IsNotNull(participant.RiotIdName);
+                    Assert.IsNotNull(participant.RiotIdTagline);
+                }
+            }
+        }
+    }
+}
