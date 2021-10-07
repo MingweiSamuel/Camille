c6d8955d440849999dac6acabb06e2852a595b98
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-10-07 09:23:07 -0700

    Add Team ID 300 for baron killing herald

--- Camille.RiotGames/src/Enums/Team.cs
+++ Camille.RiotGames/src/Enums/Team.cs
@@ -7,5 +7,8 @@
 
         /// <summary>Team ID for Summoner's Rift red side (200).</summary>
         Red = 200,
+
+        /// <summary>"killerTeamId" when Baron Nashor spawns and kills Rift Herald.</summary>
+        Other = 300,
     }
 }
