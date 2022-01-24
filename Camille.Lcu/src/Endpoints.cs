29b4a11b1d8d1be9775a4a33daf30df2a56d3cbb
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-01-23 18:58:50 -0800

    Add net framework 4.6.1 (net461) target, remove old netcoreapp2.1 code

--- Camille.Lcu/src/Endpoints.cs
+++ Camille.Lcu/src/Endpoints.cs
@@ -4,14 +4,6 @@ namespace Camille.Lcu
 {
     public abstract class Endpoints
     {
-        public static readonly HttpMethod HttpMethodPatch =
-#if USE_HTTPMETHOD_PATCH_SHIM
-            new HttpMethod("PATCH");
-#else
-            HttpMethod.Patch;
-#endif
-
-
         protected readonly ILcuApi @base;
 
         protected Endpoints(ILcuApi @base)
