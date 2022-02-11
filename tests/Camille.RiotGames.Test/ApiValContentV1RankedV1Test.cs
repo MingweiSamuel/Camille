13f99ae602121bedc0484ad07ca094df972ff7f5
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-02-10 19:48:21 -0800

    Use System.Text.Json 6.0.0, (re)enable ValContent/Ranked test
    Regen for riotapi-schema overrides:
    - https://github.com/RiotGames/developer-relations/issues/603
    - https://github.com/RiotGames/developer-relations/issues/604
    - https://github.com/RiotGames/developer-relations/issues/605

--- tests/Camille.RiotGames.Test/ApiValContentV1RankedV1Test.cs
+++ tests/Camille.RiotGames.Test/ApiValContentV1RankedV1Test.cs
@@ -3,21 +3,21 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Camille.Enums;
 using System.Linq;
+using System.Diagnostics;
 
 namespace Camille.RiotGames.Test
 {
     [TestClass]
-    [Ignore("Requires dev key.")]
-    public class ValContentV4Test : ApiTest
+    public class ValContentV1RankedV1Test : ApiTest
     {
-        public static long[] MatchIds =
-        {
-            3555874970,
-            3555881987,
-            3555758359,
-            3555745679,
-            3555760623,
-        };
+        //public static long[] MatchIds =
+        //{
+        //    3555874970,
+        //    3555881987,
+        //    3555758359,
+        //    3555745679,
+        //    3555760623,
+        //};
 
         [TestMethod]
         public async Task GetContentAsync()
@@ -37,7 +37,7 @@ namespace Camille.RiotGames.Test
 
                 Assert.IsNotNull(character.LocalizedNames.ArAE);
                 Assert.IsNotNull(character.LocalizedNames.DeDE);
-                Assert.IsNotNull(character.LocalizedNames.EnGB);
+                //Assert.IsNotNull(character.LocalizedNames.EnGB);
                 Assert.IsNotNull(character.LocalizedNames.EnUS);
                 Assert.IsNotNull(character.LocalizedNames.EsES);
                 Assert.IsNotNull(character.LocalizedNames.EsMX);
@@ -58,9 +58,9 @@ namespace Camille.RiotGames.Test
         }
 
         [TestMethod]
-        public async Task GetContentAsyncWithLocale()
+        public async Task GetContentAsyncWithLocaleAndGetLeaderboardAsync()
         {
-            var content = await Api.ValContentV1().GetContentAsync(ValPlatformRoute.AP, Locale.ja_JP);
+            var content = await Api.ValContentV1().GetContentAsync(ValPlatformRoute.AP, "ja-JP");
             Assert.IsNotNull(content);
             Assert.IsNotNull(content.Characters);
             Assert.IsTrue(0 < content.Characters.Count());
@@ -72,6 +72,10 @@ namespace Camille.RiotGames.Test
                 Assert.IsNotNull(character.AssetName);
                 Assert.IsNull(character.LocalizedNames);
             }
+
+            var act = content.Acts.First(act => act.IsActive);
+            var leaderboard = await Api.ValRankedV1().GetLeaderboardAsync(ValPlatformRoute.AP, act.Id);
+            Assert.IsTrue(0 < leaderboard.Players.Count());
         }
     }
 }
